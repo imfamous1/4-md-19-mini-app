3b2c8b0afdbe762e8e3ac307aef8143bb6655b6b
imfamous1 <107398098+imfamous1@users.noreply.github.com>
2025-02-18 21:12:42 +0300

    fix button

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def welcome_message(message):
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    web_app = WebAppInfo("https://www.ya.ru")
+    web_app = WebAppInfo("https://imfamous1.github.io/4-md-19-mini-app/")
     button = KeyboardButton(text="Открыть Mini App", web_app=web_app)
     keyboard.add(button)
     bot.send_message(message.from_user.id, "Запустить приложение",
